95b8dd6ab4b61628c69413be81f855e5bc98fb68
gekkehenkie11 <gekkehenkie11@users.noreply.github.com>
2025-08-26 17:45:12 -0400

    Update esppac_cnt.cpp

--- components/sinclair_ac/esppac_cnt.cpp
+++ components/sinclair_ac/esppac_cnt.cpp
@@ -537,6 +537,10 @@ void SinclairACCNT::send_packet()
     for (int i = 0; i < 20; i++)
          lastpacket[i] = packet[i];
     lastroomtemp = packet[42];
+
+   for (int i = 40; i < 44; i++)
+            ESP_LOGV(TAG, "Stamp1: %lx", packet[i]);
+
     
     packet.insert(packet.begin(), protocol::CMD_OUT_PARAMS_SET);
     packet.insert(packet.begin(), protocol::SET_PACKET_LEN + 2); /* Add 2 bytes as we added a command and will add checksum */
